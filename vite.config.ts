63ba58dc3655ffa27bc0cdfed7185837798c7fa0
Azharsyam22 <azharsyam22@gmail.com>
2026-03-19 19:18:47 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Prototipe-Web-UPS/', // <-- Tambahkan baris ini, pastikan namanya sama persis dengan nama repository GitHub
+  base: '/prototipe-web-ups/', // <-- Tambahkan baris ini, pastikan namanya sama persis dengan nama repository GitHub
 })
